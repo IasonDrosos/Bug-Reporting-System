984ee0628aa8bdd96abb84c1cc838b7812c5faff
Jason Drosos <jasondrosos1@gmail.com>
2019-06-10 14:41:25 +0300

    Big code refactoring

--- src/app/main/bug-list/bug-list.component.ts
+++ src/app/main/bug-list/bug-list.component.ts
@@ -1,30 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Sorting } from 'src/app/models/sorting.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostmanService } from 'src/app/Services/postman.service';
 import { faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons';
-import { faLongArrowAltDown, faCircle, faPlusCircle, faPencilAlt, faComment, faTimes, faCheck, faExclamation, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import { faLongArrowAltDown, faTimes, faCheck, faExclamation } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
-import { timeout } from 'q';
-
 
 @Component({
   selector: 'app-bug-list',
   templateUrl: './bug-list.component.html',
   styleUrls: ['./bug-list.component.css']
 })
-export class BugListComponent implements OnInit {
-
-
+export class BugListComponent implements OnInit, OnDestroy {
+  // Init fontAwesome icons for template usage
   faLongArrowAltUp = faLongArrowAltUp;
   faLongArrowAltDown = faLongArrowAltDown;
-  faCircle = faCircle;
-  faPlusCircle = faPlusCircle;
-  faPencilAlt = faPencilAlt;
-  faComment = faComment;
   faTimes = faTimes;
   faCheck = faCheck;
   faExclamation = faExclamation;
-  faSyncAlt = faSyncAlt;
+
 
 
   stateDirection = 0; // 0 none //1 asc // 2 desc
@@ -33,7 +25,7 @@ export class BugListComponent implements OnInit {
   syncTime = '5:00';
   interval;
   bugList = [];
-  collapsedRow = [];
+  collapsedRow = []; // used for comments dropdown status
   filterState = false;
 
   filter = {
@@ -43,33 +35,20 @@ export class BugListComponent implements OnInit {
     reporter: '',
     page: 0,
     sort: { column: '', direction: '' }
-  }
+  };
 
   constructor(private postmanService: PostmanService, private router: Router) { }
 
   ngOnInit() {
-
-
     this.postmanService.getTheBugs().subscribe((data: []) => {
-      this.bugList = data;
-
-      this.bugList.map(bug => {
-        if (bug.title) {
-          bug.title = bug.title.toLowerCase().charAt(0).toUpperCase() + bug.title.slice(1);
-        }
-        if (bug.status) {
-          bug.status = bug.status.toLowerCase().charAt(0).toUpperCase() + bug.status.slice(1);
-        }
-        return bug;
-      });
-
-      this.collapsedRow.length = this.bugList.length;
-      //  me to apo pano sigoureuoume oti to collapsedRow array 8a exei toses 8eseis oso kai to bugList pou erxetai
-      this.collapsedRow.fill(true);
-
       this.startTimer(300);
+      this.bugList = this.capitalizeData(data);
+      this.commentsCollapseSystem(this.bugList.length);
     });
+  }
 
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 
   sortTheBugs(column: string) {
@@ -94,20 +73,8 @@ export class BugListComponent implements OnInit {
     }
 
     this.postmanService.getBugsByFilter(this.filter).subscribe((data: []) => {
-      this.bugList = data;
-      this.collapsedRow.fill(true);
-
-
-      this.bugList.map(bug => {
-        if (bug.title) {
-          bug.title = bug.title.toLowerCase().charAt(0).toUpperCase() + bug.title.slice(1);
-        }
-        if (bug.status) {
-          bug.status = bug.status.toLowerCase().charAt(0).toUpperCase() + bug.status.slice(1);
-        }
-        return bug;
-      });
-
+      this.bugList = this.capitalizeData(data);
+      this.commentsCollapseSystem(this.bugList.length);
     });
 
   }
@@ -117,51 +84,33 @@ export class BugListComponent implements OnInit {
   }
 
   editBug(bugID) {
-    clearInterval(this.interval);
-    console.log(bugID);
     this.router.navigate(['edit', bugID]);
   }
 
   createBug() {
-    clearInterval(this.interval);
     this.router.navigate(['create']);
-
   }
-  syncBugs() { //check
+
+  syncBugs() { // check
     clearInterval(this.interval);
-
     this.startTimer(300);
+
     this.postmanService.getTheBugs().subscribe((data: []) => {
-      this.bugList = data;
-
-      this.bugList.map(bug => {
-        if (bug.title) {
-          bug.title = bug.title.toLowerCase().charAt(0).toUpperCase() + bug.title.slice(1);
-        }
-        if (bug.status) {
-          bug.status = bug.status.toLowerCase().charAt(0).toUpperCase() + bug.status.slice(1);
-        }
-        return bug;
-      });
-
+      this.bugList = this.capitalizeData(data);
+      this.commentsCollapseSystem(this.bugList.length);
     });
-    this.postmanService.getTheBugs().subscribe((data: []) => { return this.bugList = data; });
+
     this.stateDirection = 0;
     this.stateColumn = '';
     this.filter.sort = { column: '', direction: '' };
-
-
   }
 
   startTimer(counDownInSeconds: number) {
-
     this.interval = setInterval(() => {
       const minutes = Math.floor(counDownInSeconds / 60);
       const seconds = counDownInSeconds % 60;
-
       const sminutes = minutes < 10 ? '0' + minutes : minutes;
       const sseconds = seconds < 10 ? '0' + seconds : seconds;
-
 
       if (sminutes !== '0') {
         this.syncTime = minutes + ':' + sseconds;
@@ -173,7 +122,6 @@ export class BugListComponent implements OnInit {
         clearInterval(this.interval);
         this.syncBugs();
       }
-
     }, 1000);
   }
 
@@ -195,11 +143,43 @@ export class BugListComponent implements OnInit {
 
       }
     }
-    this.postmanService.getBugsByFilter(this.filter).subscribe((data: []) => { this.bugList = data; console.log(data); });
-
+    this.postmanService.getBugsByFilter(this.filter).subscribe((data: []) => {
+      this.bugList = data;
+      this.commentsCollapseSystem(this.bugList.length);
+      console.log(data);
+    });
   }
+
   filteredSearch() {
-    this.postmanService.getBugsByFilter(this.filter).subscribe((data: []) => { this.bugList = data; console.log(data); });
+    this.postmanService.getBugsByFilter(this.filter).subscribe((data: []) => {
+      clearInterval(this.interval);
+      this.startTimer(300);
+      this.bugList = data;
+      this.commentsCollapseSystem(this.bugList.length);
+    });
+  }
+
+  // Capitalize server data for proper validation and consistency
+  capitalizeData(serverData) {
+    serverData.map(bug => {
+      if (bug.title) {
+        bug.title = bug.title.toLowerCase().charAt(0).toUpperCase() + bug.title.slice(1);
+      }
+      if (bug.status) {
+        bug.status = bug.status.toLowerCase().charAt(0).toUpperCase() + bug.status.slice(1);
+      }
+      if (bug.reporter) {
+        bug.reporter = bug.reporter.toLowerCase().toUpperCase();
+      }
+      return bug;
+    });
+    return serverData;
+  }
+
+  // sigoureuoume oti to collapsedRow array 8a exei toses 8eseis oso kai to bugList pou erxetai kai to arxikopououme se katastasi true
+  commentsCollapseSystem(bugListSize: number) {
+    this.collapsedRow.length = bugListSize;
+    this.collapsedRow.fill(true);
   }
 
 
